a8c3be3f4bb0a2aa840d721d6225843ba989ee58
Ashok Cutkosky <ashok@kirjava.local>
2013-12-25 13:33:08 -0800

    first draft; should have all functionality

---
+++ XOR_learner.py
@@ -0,0 +1,142 @@
+#!/bin/python
+
+from hedonistic import *
+from random import randint,normalvariate,expovariate
+from pickle import dump
+mV = 0.001
+pF = 10.0**(-12)
+nS = 10.0**(-9)
+pA = 10.0**(-12)
+ms = 0.001
+
+
+
+def setup():
+    V_L = -74*mV
+    g_L = 25*nS
+    C = 500*pF
+    V_t = -54*mV
+    V_r = -60*mV
+    
+    inp1 = [Poisson_Spiker(50.0) for x in range(30)]
+    inp2 = [Poisson_Spiker(50.0) for x in range(30)]
+
+    
+
+    hidden_layer = [Neuron(C,g_L,V_L,normalvariate(425,200)*pA,V_t,V_r) for x in range(60)]
+
+    outputneuron = Neuron(C,g_L,V_L,normalvariate(425,200)*pA,V_t,V_r)
+
+    synapses = []
+
+    for inp in inp1:
+        r = randint(0,1)
+        V_rev_val = r*(-70)*mV
+        if(r == 0):
+            W_val = expovariate(1.0/(2.4*nS))
+        else:
+            W_val = expovariate(1.0/(45*nS))
+        for neuron in hidden_layer:
+            S = Hedonistic_Synapse(tau_e = 20*ms,W = W_val,V_rev = V_rev_val)
+            synapses.append(S)
+            neuron.inputs.append(S)
+            inp.outputs.append(S)
+
+
+    for inp in inp2:
+        r = randint(0,1)
+        V_rev_val = r*(-70)*mV
+        if(r == 0):
+            W_val = expovariate(1.0/(2.4*nS))
+        else:
+            W_val = expovariate(1.0/(45*nS))
+        for neuron in hidden_layer:
+            S = Hedonistic_Synapse(tau_e = 20*ms,W = W_val,V_rev = V_rev_val)
+            synapses.append(S)
+            neuron.inputs.append(S)
+            inp.outputs.append(S)
+
+    
+    for neuron in hidden_layer:
+        r = randint(0,1)
+        V_rev_val = r*(-70)*mV
+        if(r == 0):
+            W_val = expovariate(1.0/(2.4*nS))
+        else:
+            W_val = expovariate(1.0/(45*nS))
+        S = Hedonistic_Synapse(tau_e = 20*ms,W = W_val,V_rev = V_rev_val)
+        neuron.outputs.append(S)
+        outputneuron.inputs.append(S)
+    
+    return inp1,inp2,hidden_layer,outputneuron,synapses
+
+
+
+def trainsignal(x,y,inp1,inp2,hidden_layer,outputneuron,synapses,deltaT,trials):
+    correct = (x+y)%2
+
+    for inp in inp1:
+        inp.setstate(x)
+    for inp in inp2:
+        inp.setstate(y)
+
+        
+    intrace = []
+    outtrace = []
+    for i in range(trials):
+        trace = []
+        for inp in inp1:
+            trace.append(inp.generate(deltaT))
+        for inp in inp2:
+            trace.append(inp.generate(deltaT))
+
+        intrace.append(trace)
+
+        for neuron in hidden_layer:
+            neuron.Update(deltaT)
+        O = outputneuron.Update(deltaT)
+        outtrace.append(O)
+
+        h = 0.0
+        if(O):
+            h = 2.0*correct-1.0
+        for synapse in synapses:
+            synapse.Update(h,0.3)
+        if(i%20==0):
+            print "finished trial ",i
+    return intrace,outtrace
+    
+def train(inp1,inp2,hidden_layer,outputneuron,synapses,deltaT,trials,epochs):
+    for e in range(epochs):
+        for x in [0,1]:
+            for y in [0,1]:
+                trainsignal(x,y,inp1,inp2,hidden_layer,outputneuron,synapses,deltaT,trials)
+                print "finished epoch ",e
+
+def test(inp1,inp2,hidden_layer,outputneuron,synapses,deltaT,trials):
+    intrace = []
+    outtrace = []
+    for x in [0,1]:
+        for y in [0,1]:
+            int,outt = trainsignal(x,y,inp1,inp2,hidden_layer,outputneuron,synapses,deltaT,trials)
+            intrace += int
+            outtrace += outt
+    return intrace,outtrace
+
+
+def main():
+    print "setting up!"
+    vars = setup()
+    print "setup finished, running!"
+    train(*vars,deltaT = 1*ms,trials = 500, epochs = 200)
+
+    intrace,outtrace = test(*vars,deltaT = 1*ms,trials = 500)
+
+    fp = open("neurondump","w")
+    
+    dump([intrace,outtrace],fp)
+    fp.close()
+
+
+if __name__ == "__main__":
+    main()
--- hedonistic.py
+++ hedonistic.py
@@ -1,4 +1,8 @@
 
+#For the love of god make sure you stick to a constant unit convention.
+#For simplicity I'm going to do mks.
+
+
 
 from random import random
 from math import exp
@@ -7,12 +11,12 @@ from math import exp
 
 class Hedonistic_Synapse:
     
-    def __init__(self, initialQ = 0.0, delta_c = 0.0, tau_c = 1.0, tau_r = 1.0, tau_e = 1.0, tau_g, W=2.4, V_rev):
+    def __init__(self, initialQ = 0.0, delta_c = 0.0, tau_c = 1.0, tau_r = 1.0, tau_e = 1.0, tau_g=1.0, W=2.4, V_rev=0):
         assert(tau_c > 0)
         assert(tau_r > 0)
         assert(tau_e > 0)
         assert(tau_g > 0)
-        assert(delt_c > 0)
+        assert(delta_c >= 0)
         
 
         self.q = initialQ
@@ -20,7 +24,7 @@ class Hedonistic_Synapse:
         self.c = 0.0
         self.tau_c = tau_c
         self.tau_r = tau_r
-        self.rau_e = tau_e
+        self.tau_e = tau_e
         self.state = 'A'
         self.trace_e = 0.0
         self.tau_g = tau_g
@@ -55,7 +59,7 @@ class Hedonistic_Synapse:
         self.G += self.W*release
         self.G -= self.G/self.tau_g*deltaT
 
-        return self.release
+        return release
 
     def Update(self,h_val,eta):
         self.q += eta*h_val*self.trace_e
@@ -66,7 +70,7 @@ class Hedonistic_Synapse:
 
 class Neuron:
 
-    def __init__(self,C,g_L,V_L,I_tonic,V_t,V_r,inputs,outputs):
+    def __init__(self,C,g_L,V_L,I_tonic,V_t,V_r,inputs = [],outputs= []):
         self.C = C
         self.g_L = g_L
         self.V_L = V_L
@@ -79,15 +83,39 @@ class Neuron:
 
     def Update(self,deltaT):
         d = -self.g_L*(self.V-self.V_L)+self.I_tonic
-        d -= reduce(lambda x,y:x+y,[synapse.G*(self.V-synapse.V_rev) for synapse in inputs])
-        self.V += deltaT*d/C
+        d -= reduce(lambda x,y:x+y,[synapse.G*(self.V-synapse.V_rev) for synapse in self.inputs])
+        self.V += deltaT*d/self.C
 
         spike = False
         if(self.V>self.V_t):
             spike = True
             self.V = self.V_r
 
-        for synapse in outputs:
+        for synapse in self.outputs:
             synapse.Process(spike,deltaT)
+        return spike
 
         
+class Poisson_Spiker:
+    
+    def __init__(self,rate,state = False,outputs = []):
+        self.rate = rate
+        self.state = state
+        self.outputs = outputs
+
+    def generate(self,deltaT):
+        if(self.state == True):
+            r = random()
+            if(r<self.rate*deltaT):
+                for synapse in self.outputs:
+                    synapse.Process(True,deltaT)
+                return True
+
+        for synapse in self.outputs:
+            synapse.Process(False,deltaT)
+        return False
+    def swapstate(self):
+        self.state = not self.state
+    def setstate(self,state):
+        self.state = state
+
