dab592a3ca8b975938dcbb5f0781431d912079a9
Ashok Cutkosky <ashok@dn0a2313c8.sunet>
2014-01-08 15:03:14 -0800

    ready to test

--- NEF_tester.py
+++ NEF_tester.py
@@ -2,17 +2,33 @@
 import NEF
 from matplotlib import pyplot as plt
 from random import choice
+from math import log,exp
+from random import normalvariate,random
 
-synapses = [NEF.Synapse(inhibitory = (x%2)*2-1,initialQ = 0.0) for x in range(500)]
+def Error(x,target,deltaT):
+    Error.value += Error.grace - (target - x)**2
+    Error.value = Error.value*exp(deltaT/Error.tau)
+    return Error.value
 
-synapses = [NEF.Synapse(inhibitory = choice([1,1,1,1]),initialQ = 9.0) for x in range(200)]
+Error.grace = 0.1
+Error.value = 0.0
+Error.tau = 10*NEF.ms
 
-neurons = [NEF.NEFneuron(synapse = x) for x in synapses]
+
+
+
+#synapses = [NEF.Synapse(inhibitory = (x%2)*2-1,initialQ = 0.0) for x in range(1000)]
+
+synapses = [NEF.Synapse(inhibitory = choice([-1,1]),initialQ = 0.0) for x in range(1500)]
+
+#neurons = [NEF.NEFneuron(synapse = x) for x in synapses]
+neurons = [NEF.NEFneuron(synapse = x,e = choice([-1,1]),alpha = normalvariate(17*NEF.nA,.3*NEF.nA),J_bias = normalvariate(10*NEF.nA,.2*NEF.nA),tau_ref = normalvariate(1*NEF.ms,0.03*NEF.ms),tau_RC = normalvariate(20*NEF.ms,.4*NEF.ms)) for x in synapses]
 
 layer = NEF.NEF_layer(layer = neurons,tau_PSC = 10* NEF.ms)
 
 deltaT = 0.5*NEF.ms
 
+feedbackrate = 100
 
 x = 1.0
 
@@ -20,6 +36,20 @@ total = 0
 print 3/deltaT
 tvals = []
 xhatvals = []
+
+for a in range(100):
+    x = random()*2.0-1.0
+    print "iteration: ",a
+    print "trying x=",x
+    for z in range(int(0.5/deltaT)):
+        val = layer.Process(x,deltaT)
+        er = Error(x,x,deltaT)
+        if(random() < deltaT*feedbackrate):
+            layer.Update(er,0.2)
+
+
+x = 0.5
+
 for a in range(int(3/deltaT)):
     tvals.append(a*deltaT)
     xhatvals.append(layer.Process(x,deltaT))
