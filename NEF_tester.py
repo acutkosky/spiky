4e140924e5f276b8b4c1009238396ef0de697b05
Ashok <ashokc@stanford.edu>
2014-01-10 10:11:40 -0800

    trying various things

--- NEF_tester.py
+++ NEF_tester.py
@@ -13,6 +13,9 @@ def Error(p,target,deltaT):
 
     return Error.value
 
+def target(x):
+    return x
+
 Error.grace = 0.00
 Error.value = 0.0
 Error.tau = 0.1*NEF.ms
@@ -25,11 +28,11 @@ Error.tau = 0.1*NEF.ms
 synapses = [NEF.Synapse(inhibitory = choice([-1,1]),initialQ = 0.0) for x in range(2000)]
 
 #neurons = [NEF.NEFneuron(synapse = x) for x in synapses]
-neurons = [NEF.NEFneuron(synapse = x,e = choice([-1,1]),alpha = normalvariate(17*NEF.nA,3*NEF.nA),J_bias = normalvariate(10*NEF.nA,2*NEF.nA),tau_ref = normalvariate(1*NEF.ms,0.3*NEF.ms),tau_RC = normalvariate(20*NEF.ms,3*NEF.ms)) for x in synapses]
+neurons = [NEF.NEFneuron(synapse = x,e = choice([-1,1]),alpha = normalvariate(17*NEF.nA,4*NEF.nA),J_bias = normalvariate(10*NEF.nA,3*NEF.nA),tau_ref = normalvariate(1*NEF.ms,0.3*NEF.ms),tau_RC = normalvariate(20*NEF.ms,4*NEF.ms)) for x in synapses]
 
 layer = NEF.NEF_layer(layer = neurons,tau_PSC = 10* NEF.ms)
 
-#fp = open("neflayer_4points")
+#fp = open("neflayer_allpoints")
 #layer = load(fp)
 #fp.close()
 
@@ -44,26 +47,27 @@ print 3/deltaT
 tvals = []
 xhatvals = []
 
-x = -0.4
+x = -2
+t = target(x)
 for a in range(int(1.5/deltaT)):
     tvals.append(a*deltaT)
     xhatvals.append(layer.Process(x,deltaT))
-x = -0.2
+x = -1
 for a in range(int(1.5/deltaT)):
     tvals.append(1.5+a*deltaT)
     xhatvals.append(layer.Process(x,deltaT))
-x = 0.2
+x = 1
 for a in range(int(1.5/deltaT)):
     tvals.append(3.0+a*deltaT)
     xhatvals.append(layer.Process(x,deltaT))
-x = 0.4
+x = 2
 for a in range(int(1.5/deltaT)):
     tvals.append(4.5+a*deltaT)
     xhatvals.append(layer.Process(x,deltaT))
 
 plt.plot(tvals,xhatvals)
 plt.show()
-
+#exit()
 #plt.savefig("dataplot0_slowrate")
 #plt.show()
 c = 0
@@ -71,21 +75,25 @@ while(1):
     c+=1
     for a in range(100):
         x = choice([-2,-1,1,2])*0.2#random()*2.0-1.0
-        x = random()*2.0-1.0
+        x = random()*4.0-2.0
+        x = choice([-2,-1,0,1,2])
+        t = target(x)
         print "epoch: ",c
         print "iteration: ",a
-        print "trying x=",x
+        print "trying x= "+str(x)+" target is: "+str(t)
         for z in range(int(3.0/deltaT)):
             val = layer.Process(x,deltaT)
-            er = Error(val,x,deltaT)
+            er = Error(val,t,deltaT)
             if(random() < deltaT*feedbackrate):
                 layer.Update(er,eta)
 
-    fp = open("neflayer_allpoints","w")
+    fp = open("neflayer_5points_id_doublerange","w")
     dump(layer,fp)
     fp.close()
     x = choice([-2,-1,1,2])*0.2#random()*2.0-1.0
-    x = random()*2.0-1.0
+    x = random()*4.0-2.0
+    x = choice([-2,-1,0,1,2])
+    t = target(x)
     tvals = []
     xhatvals = []
     ervals = []
@@ -94,12 +102,11 @@ while(1):
         tvals.append(a*deltaT)
         val = layer.Process(x,deltaT)
         xhatvals.append(val)
-        ervals.append(eta*Error(val,x,deltaT))
+        ervals.append(eta*Error(val,t,deltaT))
     plt.clf()
-    plt.title("xvalue = "+str(x))
+    plt.title("xvalue = "+str(x)+" target = "+str(t))
     plt.plot(tvals,xhatvals)
     plt.plot(tvals,ervals)
-    plt.show()
-#    plt.savefig("dataplot"+str(c)+"slowrate")    
+    plt.savefig("dataplot_"+"5points_id_doublerange_"+str(c))    
 
 
