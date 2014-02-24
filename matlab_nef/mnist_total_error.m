ca455562c1fc68de3a58a1fbf4de4974cdfacf5a
Ashok Krishnan Cutkosky <ashokc@corn04.stanford.edu>
2014-02-23 19:55:10 -0800

    added mnist total error

---
+++ matlab_nef/mnist_total_error.m
@@ -0,0 +1,13 @@
+
+
+function e = mnist_total_error(layers,weights,xvals,targets)
+
+  vals = arrayfun(@(z) cellfun(@(i) eval_layer(layers{i},weights{i},z),{1,2,3,4,5,6,7,8,9,10}),xvals,'UniformOutput',false);
+
+vals = cellfun(@(x) find(x==max(x)),vals);
+num = size(vals);
+num = num(2);
+
+correct = arrayfun(@(z) vals(z)==mod(targets(z),10),1:num);
+
+e = 1-sum(correct)/num;
