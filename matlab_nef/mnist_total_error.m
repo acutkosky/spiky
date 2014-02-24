1071232e6dce40300715aef7acd09cf97ab1ed4b
Ashok Krishnan Cutkosky <ashokc@corn04.stanford.edu>
2014-02-23 20:53:42 -0800

    fixed total error so that zero predictions are counted correctly

--- matlab_nef/mnist_total_error.m
+++ matlab_nef/mnist_total_error.m
@@ -3,11 +3,12 @@
 function e = mnist_total_error(layers,weights,xvals,targets)
 
   vals = arrayfun(@(z) cellfun(@(i) eval_layer(layers{i},weights{i},z),{1,2,3,4,5,6,7,8,9,10}),xvals,'UniformOutput',false);
+vals{1}
+vals = cellfun(@(x) find(x==max(x)),vals)
 
-vals = cellfun(@(x) find(x==max(x)),vals);
 num = size(vals);
 num = num(2);
 
-correct = arrayfun(@(z) vals(z)==mod(targets(z),10),1:num);
+correct = arrayfun(@(z) mod(vals(z),10)==mod(targets(z),10),1:num);
 
 e = 1-sum(correct)/num;
